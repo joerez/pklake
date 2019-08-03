cff7848fcddb115457a060e4bed8a0e9cebb82bc
Joerez <rezenbiz@gmail.com>
2019-08-03 16:25:38 -0700

    first commit

---
+++ template-parts/header/mls.php
@@ -0,0 +1,8 @@
+<div class="pk-mls">
+
+  <div>Search By:</div>
+  <input placeholder="price" />
+  <input placeholder="bedrooms" />
+  <button>Find my home</button>
+
+</div>
---
+++ template-parts/header/nav.php
@@ -0,0 +1,9 @@
+<div class="pk-nav-bar">
+  <a href="/wordpress" class="pk-brand">PKLAKE.COM</a>
+
+  <div>
+    <a href="/wordpress" class="pk-brand">ABOUT</a>
+    <a href="/wordpress" class="pk-brand">LISTINGS</a>
+    <a href="/wordpress" class="pk-brand">LIVE</a>
+  </div>
+</div>
